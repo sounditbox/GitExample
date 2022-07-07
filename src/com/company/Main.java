1d898c333b29ebd47bab456105b74b2791c8d390
Ruslan <mutygullin@yandexlyceum.ru>
2022-07-07 15:37:22 +0500

    init commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // красный - not tracked файл не отслеживается для коммита
+        // зелёный - new         файл (строчка) был добавлен
+        // синий   - changed     файл (строчка) был изменён
+        // жёлтый  - ignored     файл игнорируется
+    }
+}
